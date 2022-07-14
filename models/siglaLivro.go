2e15aa914344128af8524370d4c32549cac8d1f0
Isaque Vieira <simiaoisaque@gmail.com>
2022-07-13 23:23:44 -0300

    Correcao do warning do modelo

--- models/siglaLivro.go
+++ models/siglaLivro.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type SiglaLivro struct {
 	gorm.Model
-	Sigla       string `JSON:sigla`
+	Sigla       string `JSON:"sigla"`
 	Fk_id_livro int
 	Livro       Livro `gorm:"foreignKey:Fk_id_livro"`
 }
